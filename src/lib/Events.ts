a4286ebda505f1c04e2c5bb7e01ec8a82ffc2d1e
YellowWall <ivarsig94@gmail.com>
2023-04-19 14:33:17 +0000

    event authorization á að vera í lagi núna

--- src/lib/Events.ts
+++ src/lib/Events.ts
@@ -56,10 +56,10 @@ export async function createEventHandler(
   res: Response,
   next: NextFunction,
 ) {
-  if(!req.cookies?.signin){
+  if(!req.body.token){
     return res.status(401).json("ekki skráður inn")
   }
-  const userInfo=jwt.decode(req.cookies.signin)
+  const userInfo=jwt.decode(req.body.token)
   if(!userInfo||!userInfo['username']){
       return res.status(401).json('ekki skráður inn');
   }
@@ -119,10 +119,10 @@ export async function updateEventHandler(
   res: Response,
   next: NextFunction,
 ) {
-  if(!req.cookies?.signin){
+  if(!req.body.token){
     return res.status(401).json("ekki skráður inn")
   }
-  const userInfo=jwt.decode(req.cookies.signin)
+  const userInfo=jwt.decode(req.body.token)
   if(!userInfo||!userInfo['username']){
       return res.status(401).json('ekki skráður inn');
   }
