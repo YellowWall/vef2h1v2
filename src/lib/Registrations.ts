11e759b74e4818bd30dde4efe0c684b2249ba12f
YellowWall <ivarsig94@gmail.com>
2023-04-19 14:43:33 +0000

    token fyrir registration köll fært í body call

--- src/lib/Registrations.ts
+++ src/lib/Registrations.ts
@@ -71,10 +71,10 @@ export async function patchRegistrationHandler(
     req: Request,
     res: Response,
     next: NextFunction,){
-    if(!req.cookies?.signin){
+    if(!req.body.token){
           return res.status(401).json('ekki skráður inn');
     }
-    const userInfo=jwt.decode(req.cookies.signin)
+    const userInfo=jwt.decode(req.body.token)
 
     const {slug,username} = req.params;
     if(!userInfo ||!(userInfo['admin']||userInfo['username']==username)){
@@ -119,10 +119,10 @@ export async function deleteRegistration(
     req:Request,
     res:Response,
     next: NextFunction){
-    if(!req.cookies?.signin){
+    if(!req.body.token){
             return res.status(401).json('ekki skráður inn');
       }
-    const userInfo=jwt.decode(req.cookies.signin)
+    const userInfo=jwt.decode(req.body.token)
     const {slug,username} = req.params
     if(!userInfo||!(userInfo['username']==username||userInfo['admin'])){
         return res.status(401).json('only administrator or this user can alter this registration')
@@ -142,10 +142,10 @@ export async function postRegistrationHandler(
     req:Request,
     res:Response,
     next:NextFunction){
-    if(!req.cookies?.signin){
-        return res.status(401).json('ekki skráður inn');
+    if(!req.body.token){
+        return res.status(401).json('ekkert token');
     }
-    const userInfo=jwt.decode(req.cookies.signin)
+    const userInfo=jwt.decode(req.body.token)
     if(!userInfo||!userInfo['username']){
         return res.status(401).json('ekki skráður inn');
     }
